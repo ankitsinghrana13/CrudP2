2d28ba496b05061f5f94097bc40a9a2e6a221e57
ankitsinghrana13 <ak9897432538@gmail.com>
2022-03-01 05:15:39 +0800

    Add project files.

---
+++ CrudP2/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using CrudP2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudP2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext dbContext;
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Employee> emp = dbContext.Employees.ToList();
+            return View(emp);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+       [HttpPost]
+       public IActionResult Create(Employee ank)
+        {
+
+            dbContext.Employees.Add(ank);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var emp = dbContext.Employees.SingleOrDefault(e => e.Id == id);
+            if (emp != null)
+            {
+                dbContext.Employees.Remove(emp);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                return RedirectToAction("Index");
+            }
+        }
+        public IActionResult Edit(int id)
+        {
+            var emp = dbContext.Employees.SingleOrDefault(e => e.Id == id);
+            return View(emp);
+        }
+        [HttpPost]
+        public IActionResult Edit(Employee emp)
+        {
+            dbContext.Employees.Update(emp);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        
+        }
+    }
+}
